6a6a0838fb46d333a635200c0f08411b2b433cfc
ayush571995 <ayush571995@outlook.com>
2022-10-29 17:10:58 +0530

    adding new hello world api

--- src/main/java/org/example/HelloController.java
+++ src/main/java/org/example/HelloController.java
@@ -11,4 +11,9 @@ public class HelloController {
     public String index() {
         return "Greetings from Spring Boot!";
     }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Greetings from Spring Boot!";
+    }
 }
